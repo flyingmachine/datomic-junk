f722422e01c60133d01823b4e07b66eecc7e4f5a
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2015-06-25 10:49:32 -0400

    following  recommendation to use tempid function in code

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.flyingmachine/datomic-junk "0.2.1"
+(defproject com.flyingmachine/datomic-junk "0.2.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
